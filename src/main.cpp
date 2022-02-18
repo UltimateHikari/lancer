052cb0bca4bde1809028976b0e3cf4f273de422e
UltimateHikari <andy@andystpad.localdomain>
2022-02-18 13:29:41 +0700

    db connected, select works

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,40 @@
 using namespace sqlite_orm;
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
 struct RapArtist {
     int id;
     std::string name;
 };
+
+class Corporation{
+    public:
+        int id;
+        std::string name;
+
+        static auto get_table(){
+            return make_table("corporation",
+            make_column("id", &Corporation::id, primary_key()),
+            make_column("name", &Corporation::name));
+        }
+};
+
+class CommodityType {
+    public:
+        int id;
+        std::string name;
+};
+
+class Commodity {
+    public:
+        int id;
+        int type_id;
+        int corp_id;
+        std::string name;
+};
+
 
 void orm_test(){
     auto storage = make_storage(":memory:",
@@ -42,7 +70,21 @@ void orm_test(){
     cout << "rap artists count = " << storage.count<RapArtist>() << " (no Kanye)" << endl;
 }
 
+void lancer_test(){
+    auto storage = make_storage("lancer.db",
+        Corporation::get_table());
+    cerr << "lancer opened" << endl;
+    storage.sync_schema();
+
+    auto rows = storage.select(columns(&Corporation::id, &Corporation::name));
+    cerr << rows.size() << endl;
+    for(auto& i: rows){
+        cerr << std::get<0>(i) << " " << std::get<1>(i) << endl;
+    }
+}
+
 int main(int, char**) {
     orm_test();
+    lancer_test();
     return 0;
 }
